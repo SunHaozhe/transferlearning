067ff3e017613a6f7c75909d3d56bd5a0a372dc6
SunHaozhe <275940200@QQ.COM>
2021-07-11 01:26:13 +0200

    update dataset transform of training data

--- code/DeepDA/data_loader.py
+++ code/DeepDA/data_loader.py
@@ -4,9 +4,7 @@ import torch
 def load_data(data_folder, batch_size, train, num_workers=0, **kwargs):
     transform = {
         'train': transforms.Compose(
-            [transforms.Resize([256, 256]),
-                transforms.RandomCrop(224),
-                transforms.RandomHorizontalFlip(),
+            [transforms.Resize([224, 224]),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225])]),
